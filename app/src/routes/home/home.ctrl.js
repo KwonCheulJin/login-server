f1e9f9732ab6f5ff3f7d69b3a547b65d4e43ca36
KwonCheulJin <KwonCheulJin@users.noreply.github.com>
2021-04-01 15:23:46 +0900

    로그인 성공

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const users = {
+  id: ['chel0001', '나개발', '김팀장'],
+  pw: ['1234', '12345', '09876']
+};
+
 const output = {
   home: (req, res) => {
     res.render('home/index');
@@ -12,7 +17,21 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const id = req.body.id,
+      pw = req.body.pw;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.pw[idx] === pw) {
+        return res.json({
+          success: true
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msq: "로그인에 실패하셨습니다."
+    });
   }
 };
 
